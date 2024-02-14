c0024db94193ee0480f88c5c7e89b0cff9a85323
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-02-13 21:49:53 -0300

    Update streamlitCONTAGEM.py

--- streamlitCONTAGEM.py
+++ streamlitCONTAGEM.py
@@ -38,21 +38,12 @@ st.subheader("Contagem por tipo de laudo:")
 for trecho, contagem in contagem_por_trecho.items():
     st.write(f"{trecho}: {contagem}")
 
-# Adicione esta linha para depuração
-print("Contagem por trecho:", contagem_por_trecho)
-
 # Criando gráfico de pizza
 fig, ax = plt.subplots()
-
-# Definindo dados para o gráfico de pizza
 labels = contagem_por_trecho.keys()
 sizes = contagem_por_trecho.values()
 colors = plt.cm.tab20c.colors[:len(labels)]
 
-# Adicione esta linha para depuração
-print("Tamanhos das fatias:", sizes)
-
-# Adicionando gráfico de pizza
 wedges, texts, autotexts = ax.pie(sizes, labels=None, colors=colors, autopct='%1.1f%%', startangle=140, pctdistance=0.85, explode=[0.05] * len(labels))
 
 ax.set_title("Distribuição dos laudos de supervisão ocupacional", pad=20)
